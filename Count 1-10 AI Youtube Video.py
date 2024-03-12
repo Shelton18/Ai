ceb21ff9a1906ebc8e1937be051827240f6db09b
Shelton18 <SheltonLaDanien@gmail.com>
2024-03-12 13:52:52 -0400

    Added for loop called Count- Printing numbers 5-20

--- Count 1-10 AI Youtube Video.py
+++ Count 1-10 AI Youtube Video.py
@@ -2,3 +2,9 @@
 # Date: 3.12.2024
 # Program: Count 1-10
 # Resource:https://www.youtube.com/watch?v=jRAAaDll34Q
+
+print("hi")
+
+# Loop from 1 to 10 and print each number
+for i in range(5, 21):
+    print(i)
