4b0cf8c60648d0bf4cc9e902ce8d6fbf4863d5ad
Shelton18 <SheltonLaDanien@gmail.com>
2024-03-14 13:33:35 -0400

    User password hidden - Run in Terminal

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,38 +3,9 @@
 # Program: Password Generator
 # Resources: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
-import hashlib
-import os
 
+import getpass
 
-def hash_password(password, salt):
-    # Combine password and salt
-    salted_password = password.encode() + salt.encode()
+password = getpass.getpass("Enter your password: ")
 
-    # Hash the combined string using SHA-256 algorithm
-    hashed_password = hashlib.sha256(salted_password).hexdigest()
-
-    return hashed_password
-
-
-def generate_salt():
-    # Generate a random salt
-    return os.urandom(16).hex()
-
-
-def main():
-    # Accept password input from user
-    password = input("Enter your password: ")
-
-    # Generate a salt
-    salt = generate_salt()
-
-    # Hash the password with the salt
-    hashed_password = hash_password(password, salt)
-
-    # Print the hashed password
-    print("Hashed Password:", hashed_password)
-
-
-if __name__ == "__main__":
-    main()
+print("Password entered:", password)
